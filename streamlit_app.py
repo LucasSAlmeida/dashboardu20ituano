b29eb574cc64580ed9aa3accd61bab12d414e63b
Lucas Almeida <87536441+LucasSAlmeida@users.noreply.github.com>
2024-11-06 16:12:52 -0300

    Update streamlit_app.py
    Mudei o código para que os dados de passes e cruzamentos mostrem a origem e o destino das ações, com pontos e setas no campo.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ from utils.DrawPitch import create_pitch_plotly
 
 # Carregar os dados
 df = pd.read_csv('https://raw.githubusercontent.com/LucasSAlmeida/dados/main/eventos.csv')
-df_grouped_by = df.groupby(['evento', 'posicao'])[['x']].count()
-df_grouped_by.reset_index(inplace=True)
+
 
 # Título da aplicação
 st.title("Ituano Futebol Clube - Sub20")
@@ -21,45 +20,48 @@ selected_action = st.selectbox('Selecione uma ação', df.evento.unique())
 st.subheader('Gráfico de Ações em Campo')
 filtered_df = df[(df['posicao'] == selected_player) & (df['jogo'] == selected_game)]
 
-if selected_action == 'Passe certo':
-    filtered_passes = filtered_df[(filtered_df['evento'] == 'Passe certo')]
+if selected_action in ['Passe certo','Passe errado','Cruzamento']:
+    filtered_passes = filtered_df[(filtered_df['evento'] == selected_action)]
     
-    # Criar figura do campo
-    pitch_figure = create_pitch_plotly(120, 80, 'yards', 'black',df=filtered_passes)
-    
-    # Adicionar setas de passes
-    for _, row in filtered_passes.iterrows():
-        pitch_figure.add_trace(
-            go.Scatter(
-                x=[row['x']],
-                y=[row['y']],
-                mode='lines+markers',
-                line=dict(color='black', width=2),
-                marker=dict(color='red', size=6),
-                showlegend=False,
-                hoverinfo='none',
+    if not filtered_passes.empty:
+        # Criar figura do campo
+        pitch_figure = create_pitch_plotly(120, 80, 'yards', 'black', df=filtered_passes)
+        
+        # Adicionar setas de passes
+        for _, row in filtered_passes.iterrows():
+            pitch_figure.add_trace(
+                go.Scatter(
+                    x=[row['x']],
+                    y=[row['y']],
+                    mode='lines+markers',
+                    line=dict(color='black', width=2),
+                    marker=dict(color='red', size=6),
+                    showlegend=False,
+                    hoverinfo='none',
+                )
             )
-        )
-        pitch_figure.add_annotation(
-            x=row['x2'], 
-            y=row['y2'], 
-            ax=row['x'], 
-            ay=row['y'],
-            xref="x", 
-            yref="y",
-            axref="x", 
-            ayref="y",
-            showarrow=True,
-            arrowhead=2,
-            arrowsize=1.5,
-            arrowwidth=2,
-            arrowcolor='black'
-        )
+            pitch_figure.add_annotation(
+                x=row['x2'], 
+                y=row['y2'], 
+                ax=row['x'], 
+                ay=row['y'],
+                xref="x", 
+                yref="y",
+                axref="x", 
+                ayref="y",
+                showarrow=True,
+                arrowhead=2,
+                arrowsize=1.5,
+                arrowwidth=2,
+                arrowcolor='black'
+            )
+        st.plotly_chart(pitch_figure)
+    else:
+        st.write(f"Não foram encontrados dados sobre '{selected_action}' para o jogador {selected_player} no jogo {selected_game}.")
 else:
     filtered_action_df = filtered_df[(filtered_df['evento'] == selected_action)]
     pitch_figure = create_pitch_plotly(120, 80, 'yards', 'black', filtered_action_df)
-
-st.plotly_chart(pitch_figure)
+    st.plotly_chart(pitch_figure)
 
 #2. Dados da Temporada
 st.subheader('Gráfico de Barras de Ações na temporada')
@@ -93,6 +95,3 @@ action_counts.columns = ['Evento', 'Quantidade']
 
 # Exibir a tabela
 st.dataframe(action_counts)
-
-
-
